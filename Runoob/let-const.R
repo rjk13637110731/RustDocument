ed7e20882e9deb412dfb4ba04a0ad76eaba0f467
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-07 08:20:00 +0800

    Create let-const.R

---
+++ Runoob/let-const.R
@@ -0,0 +1,12 @@
+fn main(){
+    let mut a = 123;
+    a = 456;
+    const b : i32 = 145;
+    println!("{}",a);
+    println!("{}",b);
+
+    let x = 5;
+    let x = x + 5;
+    let x = x * 5;
+    println!("{}",x);
+}
