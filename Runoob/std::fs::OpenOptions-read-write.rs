339e1fc0479545204205ee95072c43b2c18bebc5
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-10 11:34:33 +0800

    Create std::fs::OpenOptions-read-write.rs

---
+++ Runoob/std::fs::OpenOptions-read-write.rs
@@ -0,0 +1,8 @@
+use std::io::prelude::*;
+use std::fs::OpenOptions;
+
+fn main() -> std::io::Result<()> {
+    let mut file = OpenOptions::new().read(true).write(true).open(".\\text.txt")?;
+    file.write(b"COVER")?;
+    Ok(())
+}
