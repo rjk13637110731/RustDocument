b23f596bb2ce528c36e53e0ad75da5d021ce36c7
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-10 20:29:18 +0800

    Create to_string().rs

---
+++ Runoob/to_string().rs
@@ -0,0 +1,10 @@
+fn main(){
+    let string = String::new();
+    println!("{}",string);
+    let one = 1.to_string();
+    println!("{}",one);
+    let float = 3.14.to_string();
+    println!("{}",float);
+    let slice = "Slice".to_string();
+    println!("{}",slice);
+}
